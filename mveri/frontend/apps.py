6d42fa454cd5b389056730092d26750500cd39c6
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-07-19 12:44:48 +0000

    update and fix

--- mveri/frontend/apps.py
+++ mveri/frontend/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class FrontendConfig(AppConfig):
+    """
+    Configuration for the Frontend application.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "frontend"
