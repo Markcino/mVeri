35fc01f72e31e3f4e2711009169ef745246eb3d3
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-08-23 16:27:11 +0000

    fix the bug for create a user

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,32 @@
 from django.db import models
+from . managers import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
 
 # Create your models here.
 
+def CustomUser(AbstractBaseUser, PermissionsMixin):
+    username:None
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(_('staff'), default=False)
+    is_superuser = models.BooleanField(_('superuser'), default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    @property
+    def get_name(self):
+        return self.first_name+" "+self.last_name
+
+    def __str__(self):
+        return self.email
