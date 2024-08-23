ba1466c61f52326939f6e31a6d929b43072f11f9
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-08-23 13:05:26 +0000

    Setting up the project

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,40 @@
-"""
-ASGI config for config project.
+# """
+# ASGI config for config project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+# It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+# """
+
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+# application = get_asgi_application()
+
 
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": get_asgi_application(),
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+            ])
+        )
+    ),
+})
--- frontend/apps.py
+++ frontend/apps.py
@@ -1,8 +1,6 @@
 from django.apps import AppConfig
 
+
 class FrontendConfig(AppConfig):
-    """
-    Configuration for the Frontend application.
-    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "frontend"
